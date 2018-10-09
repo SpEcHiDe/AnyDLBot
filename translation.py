01075b6260ac479ccf65d1fa46be7b250ce18008
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-10-09 19:28:18 +0530

    fixes

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 
 class Translation(object):
-    START_TEXT = "Please send me any video link. Supported Sites: https://rg3.github.io/youtube-dl/supportedsites.html"
-    ABS_TEXT = "please subscribe @MalayalamTrollVoice #ForTheGreaterGood "
+    START_TEXT = "Please send me any video link. Supported Sites: https://rg3.github.io/youtube-dl/supportedsites.html \nSource Code: https://github.com/SpEcHiDe/AnyDLBot"
+    ABS_TEXT = " #ForTheGreaterGood "
     UPGRADE_TEXT = "No upgrades!"
