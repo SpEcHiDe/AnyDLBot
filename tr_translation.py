bf69d7c7addd4657eac543423f49342922733316
M.Furkan <byazade@yandex.com>
2019-02-06 14:08:14 +0300

    Added Turkish Language

---
+++ tr_translation.py
@@ -0,0 +1,50 @@
+class Translation(object):
+    START_TEXT = "/help komutunu kullanarak beni nasıl kullanacağını öğrenebilirsin\n/upgrade komutunu kullanarak premium özelliklerine sahip olabilirsin\nKaynak Kod: https://github.com/SpEcHiDe/AnyDLBot \n© @AnyDLBot\n Çeviri @By_Azade tarafından yapılmıştır."
+    RENAME_403_ERR = "Üzgünüm. Dosyayı isimlendirmek için yeterli izinlerin yok"
+    ABS_TEXT = " Lütfen bencil olma"
+    UPGRADE_TEXT = "@AnyDLBot <a href='https://t.me/SpEcHlDe/599'>Ücretler</a>\nÜcreti ödedikten sonra  @shrimadhabot  a veya @SpEcHIDe ye ödemenin yapıldığına dair ekran görüntüsü atmalısın"
+    FORMAT_SELECTION = "İstediğiniz formatı seçin: <a href='{}'>dosya boyutu yaklaşık olaraktır</a> \nÖzel küçük resim ayarlamak istiyorsanız, aşağıdaki düğmelerden birine dokunduktan önce veya sonra fotoğraf gönderin."
+    NOYES_URL = "@GetPublicLinkBot URL tespit edildi. Lütfen servisi kötüye kullanma!"
+    DOWNLOAD_START = "indirmeye çalışıyor"
+    UPLOAD_START = "yüklenmeye çalışılıyor"
+    RCHD_BOT_API_LIMIT = "izin verilen maksimum boyuttan (50MB) daha büyük Bununla birlikte, yüklemeye çalışıyorum."
+    RCHD_TG_API_LIMIT = "Afedersiniz. Ancak, Telegram API kısıtlamaları nedeniyle 1,5 GB'den büyük dosyaları yükleyemiyorum."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Lütfen yararlı bulursanız beni değerlendirin. https://t.me/tlgrmcbot?start=anydl_bot-bot"
+    NOT_AUTH_USER_TEXT = "Lütfen üyeliğinizi /upgrade komutunu kullanarak yükseltin."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Algılanan Dosya Boyutu: {}. Ücretsiz Kullanıcılar yalnızca yükleme yapabilir: {}\nLütfen üyeliğinizi /upgrade komutunu kullanarak yükseltin.\nEğer botta bir hata bulursanız buradan benimle iletişime geçebilirsiniz <a href='https://telegram.dog/ShrimadhaVahdamirhS'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Özel video / dosya küçük resmi kaydedildi. Bu görüntü videoda / dosyada kullanılacak."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅  Özel küçük resim başarıyla temizlendi."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Medya başarıyla temizlendi."
+    SAVED_RECVD_DOC_FILE = "Belge Başarıyla İndirildi"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Özel bir küçük resim bulunamadı."
+    NO_VOID_FORMAT_FOUND = "URL’yi kontrol edebilir misiniz? URL’den video biçimini tespit edemiyorum. Eğer botta bir hata bulursanız buradan bildirebilirsiniz https://github.com/spechide/AnyDLBot/issues"
+    USER_ADDED_TO_DB = "{} 'E kadar {}' a <a href='tg://user?id={|'> {} </a> kullanıcısı eklendi.
+    CURENT_PLAN_DETAILS = """ Mevcut plan detayları
+--------
+Telegram ID: <code>{}</code>
+Plan ismi: <a href='https://t.me/SpEcHlDe/599'>{}</a>
+Bitiş tarihi: {}"""
+    HELP_USER = """Yapabileceğim çok şey var:
+👉 Tüm Desteklenen Video Formatları https://rg3.github.io/youtube-dl/supportedsites.html
+👉 Herhangi bir HTTP bağlantısından dosya olarak yükleyin
+👉 Herhangi bir Telegram ortamına izlenebilir Videoya Dönüştürme
+👉 Dosyaları Yeniden İsimlendirme, Özel Küçük Resim Ekleme
+👉 Herhangi bir Telegram dosyasının Yüksek Hızlı Doğrudan İndirme Bağlantısını Alın
+--------
+/me komutunu kullanarak şuanki mevcut plan detaylarını görüntüle"""
+    REPLY_TO_DOC_GET_LINK = "Yüksek Hızlı Doğrudan İndirme Bağlantısı almak için bir Telegram ortamına yanıtlayın"
+    REPLY_TO_DOC_FOR_C2V = " Dönüştürmek için bir Telegram ortamına cevap verin"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Özel küçük resim desteğiyle (/rename) yeniden adlandırmak için bir Telegram ortamına yanıt verin"
+    AFTER_GET_DL_LINK = "Doğrudan indirme linki <a href='{}'>oluşturuldu</a> {} gün sonra süresi dolacak.\n© @AnyDLBot"
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Söz Dizimi: /trim HH:MM:SS [HH:MM:SS]"""
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "İlk olarak yerel ortamıma indirebilmek için herhangi bir ortama /downloadmedia komutunu gönderin. \nŞu anda indirilmiş olan medyayı tanımak için  /storageinfo komutunu kullanın."
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Süresi: {}\n/clearffmpegmedia komutunu kullanarak bu medyayı benim yerel ortamımdan silebilirsin.\n/trim HH:MM:SS [HH:MM:SS] komutunu kullanarak videoyu veya fotoğrafı küçük boyuna indirebilirsin kesebilirsin."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "Kaydedilmiş bir medya zaten var. Lütfen/storageinfo komutunu kullanarak şu anki medya detaylarını öğrenebilirsin."
+    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Gömülü altyazı çıkarmak için bir HTTP bağlantısına cevap verin"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Özel bir küçük resim üretmek için  /generatecustomthumbnail komutunu kullanarak medyayı cevapla."
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Medya Albümü yalnızca iki fotoğraf içermelidir. Lütfen medya albümünü tekrar gönderin ve tekrar deneyin veya bir albümde sadece iki fotoğraf gönderin."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "URL formatı hatalıdır. Url şu şekilde olmalıdır http:// veya https://. Belirli bir dosyayı linki gönderirken isimlendirmek için bu formatı kullanın <link> | dosya uzantısı"
+    ABUSIVE_USERS = "Bu botu kullanmana izin verilmiyor. Bunun bir hata olduğunu düşünüyorsanız, buradan  <a href='https://telegram.dog/ShrimadhaVahdamirhS'>@SpEcHlDe</a> iletişime geçin ve bu engellemeyi kaldırın."
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
