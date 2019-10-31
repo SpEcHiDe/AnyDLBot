13a16b8cb0922c277c309e05578310827288f599
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-10-31 18:27:45 +0530

    fix broken /getlink

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -96,12 +96,10 @@ async def get_link(bot, update):
         else:
             logger.info(t_response)
             t_response_arry = t_response.decode("UTF-8").split("\n")[-1].strip()
-            shorten_api_url = "http://ouo.io/api/{}?s={}".format(Config.OUO_IO_API_KEY, t_response_arry)
-            adfulurl = requests.get(shorten_api_url).text
         await bot.edit_message_text(
             chat_id=update.chat.id,
-            text=Translation.AFTER_GET_DL_LINK.format(adfulurl, max_days),
-            parse_mode=pyrogram.ParseMode.HTML,
+            text=Translation.AFTER_GET_DL_LINK.format(t_response_arry, max_days),
+            parse_mode="html",
             message_id=a.message_id,
             disable_web_page_preview=True
         )
