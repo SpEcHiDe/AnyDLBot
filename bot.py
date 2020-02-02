c0952723988993da19c3d8c7e89c5b68ddf58875
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-02-02 17:49:24 +0530

    Change Auth user

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(7351948)
+    Config.AUTH_USERS.add(683538773)
     app.run()
