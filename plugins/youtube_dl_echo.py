30097295500438ab0b6b27e926863bb47f96547c
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-02-02 18:52:47 +0530

    Added Banned User to ban Unwanted Members
    Added Banned User to ban Unwanted Members

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -33,7 +33,7 @@ from helper_funcs.help_uploadbot import DownLoadFile
 
 @pyrogram.Client.on_message(pyrogram.Filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
