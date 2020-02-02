ef2430e57da4e6b9bf21ee6173060a2a075c04e9
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-02-02 18:48:54 +0530

    Added Banned User to ban Unwanted Members
    Added Banned User to ban Unwanted Members

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -39,7 +39,7 @@ from PIL import Image
 
 @pyrogram.Client.on_callback_query()
 async def button(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.message.chat.id,
             message_ids=update.message.message_id,
