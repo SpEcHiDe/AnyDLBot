171cdb8e98bd2850739d51dab912bbed06537348
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-02-02 19:03:36 +0530

    Change STATRT

--- translation.py
+++ translation.py
@@ -10,7 +10,7 @@ Support Group : @InFoTelGroup
 © @SpEcHlDe , @TGBotsZ & @CWProjects"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "</b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy OWn URL Bot.</a>"
+    UPGRADE_TEXT = "</b>👉 Create own Clone Bot </b>"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
