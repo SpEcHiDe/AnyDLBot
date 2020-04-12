b687ec4b55bc39ffd8f7b688d0571272c49d6f2d
noupus <63485872+noupus@users.noreply.github.com>
2020-04-12 20:42:56 +0530

    Update and rename translation.py to 1translation.py

--- 1translation.py
+++ 1translation.py
@@ -1,7 +1,5 @@
 class Translation(object):
-    START_TEXT = """This is an @AnyDLBot clone created using the Open Source code available.
-Source Code: https://github.com/SpEcHiDe/AnyDLBot
-© @AnyDLBot"""
+    START_TEXT = """This is a Telegram URL Upload Bot! © @TorrentStore"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "This is an @AnyDLBot clone created using the Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nStart @AnyDLBot to know Paid Plans!"
