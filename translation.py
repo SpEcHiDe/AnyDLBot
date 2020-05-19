baf25b5c34930fda113e14482ffa916e67cbe7a5
techtelegram <65564336+techtelegram@users.noreply.github.com>
2020-05-19 23:11:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -26,13 +26,8 @@ This is a Telegram Rename Bot!
     
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension.
-   
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
-
 --------
-
-Support Group : @InFoTelGroup
-© @SpEcHlDe @TGBotsZ"""
+"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
