6e2cd862933defe560048d856d7eba56cc354240
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-06-24 11:45:48 +0530

    Fixed BANNED_USERS

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -34,11 +34,7 @@ from helper_funcs.help_uploadbot import DownLoadFile
 @pyrogram.Client.on_message(pyrogram.Filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
+        await update.reply_text("You are B A N N E D")
         return
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/echo")
