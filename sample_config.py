9319d8a9ad3e164f5be935d3dc1297e1394f55f5
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-06-24 11:44:18 +0530

    Fixed BANNED_USERS

--- sample_config.py
+++ sample_config.py
@@ -12,7 +12,7 @@ class Config(object):
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
     # Banned Unwanted Members..
-    BANNED_USERS = []
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
