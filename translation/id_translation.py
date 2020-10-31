1238e499ada4b92ed64b6997099506fdfbd428f7
Hafitz21 <71178188+HafitzSetya@users.noreply.github.com>
2020-10-31 19:31:54 +0700

    Add Indonesian translation

---
+++ translation/id_translation.py
@@ -0,0 +1,86 @@
+class Translation:
+    START_TEXT = """Ini adalah klon @AnyDLBot yang dibuat menggunakan kode Sumber Terbuka yang tersedia..
+Kode sumber: https://github.com/SpEcHiDe/AnyDLBot
+© @AnyDLBot"""
+    RENAME_403_ERR = "Maaf. Anda tidak diizinkan untuk mengganti nama file ini."
+    ABS_TEXT = " Tolong jangan egois."
+    UPGRADE_TEXT = "Ini adalah tiruan @AnyDLBot yang dibuat menggunakan Open Source <a href='https://github.com/SpEcHiDe/AnyDLBot'>code</a>. \nMulailah @AnyDLBot untuk mengetahui Paket Berbayar!"
+    FORMAT_SELECTION = "Pilih format yang diinginkan: <a href='{}'>ukuran file mungkin hanya perkiraan</a> \nJika Anda ingin menyetel thumbnail khusus, kirim foto sebelum atau cepat setelah mengetuk salah satu tombol di bawah ini. \nAnda dapat menggunakan /deletehumbnail untuk menghapus thumbnail yang dibuat secara otomatis."
+    SET_CUSTOM_USERNAME_PASSWORD = """Jika Anda ingin mendownload video premium, sediakan dalam format berikut:
+URL | nama file | nama pengguna | kata sandi"""
+    NOYES_URL = "@robot URL terdeteksi. Mohon gunakan https://shrtz.me/PtsVnf6 dan berikan saya URL cepat sehingga saya dapat mengunggah ke Telegram, tanpa memperlambat saya untuk pengguna lain."
+    DOWNLOAD_START = "mencoba mengunduh"
+    UPLOAD_START = "mencoba mengunggah"
+    RCHD_BOT_API_LIMIT = "ukuran lebih besar dari ukuran maksimum yang diperbolehkan (50MB). Namun demikian, mencoba mengupload."
+    RCHD_TG_API_LIMIT = "Saya tidak dapat mengunggah file yang lebih besar dari 1,95 GB karena batasan API Telegram."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Beri nilai saya jika menurut Anda saya berguna. https://t.me/tlgrmcbot?start=anydl_bot-bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Diunduh dalam {} detik. \nBeri nilai saya jika menurut Anda saya berguna. https://t.me/tlgrmcbot?start=anydl_bot-bot \nDiupload di {} detik."
+    NOT_AUTH_USER_TEXT = "Silakan /upgrade langganan Anda."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Ukuran File Terdeteksi: {}. Y: {}\nSilakan /upgrade langganan Anda.\nJika menurut Anda ini bug, harap hubungi <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Gambar mini video / file khusus disimpan. Gambar ini akan digunakan di video / file."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Thumbnail kustom berhasil dihapus."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media berhasil dibersihkan."
+    SAVED_RECVD_DOC_FILE = "Dokumen Berhasil Diunduh."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Tidak ditemukan ThumbNail Kustom."
+    NO_VOID_FORMAT_FOUND = "tidak ada yang akan membantumu\n<b>YouTubeDL</b> kata: {}"
+    USER_ADDED_TO_DB = "Pengguna <a href='tg://user?id={}'>{}</a> ditambahkan ke {} untuk {}."
+    CURENT_PLAN_DETAILS = """Detail rencana saat ini
+--------
+Telegram ID: <code>{}</code>
+Nama rencana: <a href='https://t.me/SpEcHlDe/599'>{}</a>
+Kadaluarsa pada: {}"""
+    HELP_USER = """Ada beberapa hal yang bisa saya lakukan:
+👉 <a href="https://t.me/SpEcHlDe/608">Semua Format Video / File yang Didukung, dengan nama file kustom dan dukungan thumbnail kustom</a>
+👉 <a href="https://t.me/SpEcHlDe/609">Unggah sebagai file dari tautan HTTP apa pun, dengan dukungan thumbnail kustom</a>
+👉 <a href="https://telegram.dog/AnyDLBot">Konversi ke Video Streaming, file Telegram apa pun</a>
+👉 <a href="https://telegram.dog/AnyDLBot">Convert To Telegram Audio, media dikirim sebagai Telegram Dokumen</a>
+👉 <a href="https://t.me/SpEcHlDe/610">Namai ulang file Telegram, dengan dukungan thumbnail kustom</a>
+👉 <a href="https://t.me/SpEcHlDe/625">Hasilkan Gambar Kecil Kustom dengan mengirimkan dua foto dalam Album Media</a>
+👉 <a href="https://t.me/SpEcHlDe/653">Pangkas video besar</a>, dan <a href="https://t.me/SpEcHlDe/652">Ambil Tangkapan Layar</a> dari file media Telegram.
+👉 <a href="https://t.me/SpEcHlDe/657">Ekstrak media Telegram terkompresi</a>
+👉 <a href="https://t.me/SpEcHlDe/660">Dapatkan stiker Telegram sebagai media Telegram yang dapat diunduh</a>
+--------
+Kirim /me untuk mengetahui detail paket saat ini"""
+    REPLY_TO_DOC_GET_LINK = "Balas ke media Telegram untuk mendapatkan Tautan Unduhan Langsung Berkecepatan Tinggi"
+    REPLY_TO_DOC_FOR_C2V = "Balas ke media Telegram untuk mengkonversi"
+    REPLY_TO_DOC_FOR_SCSS = "Balas media Telegram untuk mendapatkan tangkapan layar"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Balas media Telegram ke /rename dengan dukungan thumbnail kustom"
+    AFTER_GET_DL_LINK = "Tautan Langsung <a href='{}'>Dihasilkan</a> sah untuk {} hari.\n© @AnyDLBot"
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Sintaksis: /trim HH:MM:SS [HH:MM:SS]"""
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "Pertama kirim /downloadmedia ke media apa saja agar bisa diunduh ke lokal saya. \nKirim /storageinfo untuk mengetahui media yang sedang diunduh."
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Durasi Video: {}\nKirim /clearffmpegmedia untuk menghapus media ini, dari penyimpanan saya.\nKirim /trim HH:MM:SS [HH:MM:SS] untuk menghasilkan foto / video kecil, dari media di atas."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "Media yang disimpan sudah ada. Silakan kirim /storageinfo untuk mengetahui detail media saat ini."
+    USER_DELETED_FROM_DB = "Pengguna <a href='tg://user?id={}'>{}</a> dihapus dari DataBase."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Balas ke media Telegram (MKV), untuk mengekstrak aliran tersemat"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Balas /generatecustomthumbnail ke album media, untuk menghasilkan thumbnail kustom"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Album Media hanya boleh berisi dua foto. Harap kirim ulang album media, lalu coba lagi, atau kirim hanya dua foto dalam satu album."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "Format URL salah. pastikan url Anda dimulai dengan salah satunya http:// atau https://. Anda dapat mengatur nama file kustom menggunakan format link | nama_file.perpanjangan"
+    ABUSIVE_USERS = "Anda tidak diizinkan menggunakan bot ini. Jika menurut Anda ini adalah kesalahan, harap periksa /me untuk menghapus batasan ini."
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    EXTRACT_ZIP_INTRO_ONE = "Kirim file terkompresi terlebih dahulu, lalu balas /unzip perintah ke file."
+    EXTRACT_ZIP_INTRO_THREE = "Menganalisis file yang diterima. ⚠️ Ini mungkin membutuhkan waktu. Harap bersabar. "
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    EXTRACT_ZIP_ERRS_OCCURED = "Maaf. Kesalahan terjadi saat memproses file yang dikompresi. Harap periksa kembali semuanya dua kali, dan jika masalah terus berlanjut, laporkan ke <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_STEP_TWO = """Pilih nama_file untuk diunggah dari opsi di bawah ini.
+Anda dapat menggunakan /rename perintah setelah menerima file untuk mengganti namanya dengan dukungan thumbnail kustom."""
+    CANCEL_STR = "Proses Dibatalkan"
+    ZIP_UPLOADED_STR = "Diupload {} file dalam {} detik"
+    FREE_USER_LIMIT_Q_SZE = """Tidak Dapat Memproses.
+Pengguna gratis hanya 1 permintaan per 30 menit.
+/upgrade atau Coba 1800 detik kemudian."""
+    SLOW_URL_DECED = "Astaga, sepertinya URL sangat lambat. Karena Anda mengacaukan rumah saya, saya tidak berminat mengunduh file ini. Sementara itu, mengapa Anda tidak mencobanya:==> https://shrtz.me/PtsVnf6 dan berikan saya URL cepat sehingga saya dapat mengunggah ke Telegram, tanpa memperlambat saya untuk pengguna lain."
+    IFLONG_FILE_NAME = """Batas Nama File yang diperbolehkan oleh Telegram adalah {alimit} karakter.
+Nama file yang diberikan memiliki {num} karakter.
+
+<b>Esai Tidak diperbolehkan dalam nama file Telegram!</b>
+©️ <code>@ReNameBot</code>
+Harap persingkat nama file Anda dan coba lagi!"""
+    YTDL_ERROR_MESSAGE = (
+        "tolong laporkan masalah ini pada https://yt-dl.org/bug . "
+        "Pastikan Anda menggunakan versi terbaru; Lihat "
+        " https://yt-dl.org/update  tentang cara memperbarui. "
+        "Pastikan untuk memanggil youtube-dl dengan --verbose flag "
+        "dan menyertakan keluaran lengkapnya."
+    )
+    ISOAYD_PREMIUM_VIDEOS = "video hanya tersedia untuk pengguna terdaftar"
