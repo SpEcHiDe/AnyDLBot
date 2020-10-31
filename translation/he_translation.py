f286ae6a1b1188c9dc991b89276180f4119bbfce
iiiiii1wepfj <itaykotik@gmail.com>
2020-10-31 10:37:20 +0200

    add hebrew translation
    add hebrew translation

---
+++ translation/he_translation.py
@@ -0,0 +1,86 @@
+class Translation:
+    START_TEXT = """זה שיבוט של @AnyDLBot פרוייקט קוד פתוח.
+הקוד: https://github.com/SpEcHiDe/AnyDLBot
+© @AnyDLBot"""
+    RENAME_403_ERR = "שגיאה לא הצלחתי לשנות את שם הקובץ"
+    ABS_TEXT = " בבקשה אל תהיה אנוכי."
+    UPGRADE_TEXT = "זה שיבוט של @AnyDLBot פרוייקט קוד פתוח <a href='https://github.com/SpEcHiDe/AnyDLBot'>הקוד</a>. \nתתחיל את  @AnyDLBot בפרטי בשביל לראות את התכניות!"
+    FORMAT_SELECTION = "בחר את הפורמט שאתה רוצה: <a href='{}'>גודל הקובץ צפוי להיות</a> \nאם תרצה תמונה ממוזערת בקובץ, שלח את התמונה אחרי לחיצה על אחד מהכפתורים למטה.\nאתה יכול להשתמש בפקודה /deletethumbnail בשביל למחוק את התמונה הממוזער."
+    SET_CUSTOM_USERNAME_PASSWORD = """אם אתה רוצה להוריד את זה בפרימיום אז תכתוב את זה בפורמט הזה:
+כתובת אתר | שם הקובץ | שם משתמש | סיסמה"""
+    NOYES_URL = "@robot קישור זוהה. בבקשה תשתמש ב https://shrtz.me/PtsVnf6 בשביל לקבל קישור מהיר שאני יוכל להעלות לטלגרם, בשביל שאאט עבור משתמשים אחרים."
+    DOWNLOAD_START = "מנסה להוריד"
+    UPLOAD_START = "מנסה להעלות"
+    RCHD_BOT_API_LIMIT = "הקובץ גדול יותר מהמגבלה של bot api (50MB). עדיין מנסה להעלות."
+    RCHD_TG_API_LIMIT = "לא יכול להעלות את הקובץ בגלל שהוא גדול יותר מ 1.95GB זה יותר ממגבלת הטלגרם."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "אם אני הייתי שימושי אתה יכול לדרג אותי. https://t.me/tlgrmcbot?start=anydl_bot-bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "מוריד תוך {} שניות. \nאתה יכול לדרג אותי אם הייתי שימושי. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "אתה יכול להשתמש בפקודה /upgrade בשביל לשדרג את התכנית שלך."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "זיהיתי את הגודל של הקובץ: {}. משתמשים בתכנית חינמית יכולים להעלות עד: {}\nאתה יכול להשתמש בפקודה  /upgrade בשביל לשדרג את התכנית שלך.\nאם אתה חושב שזאת תקלה אתה יכול לפנות ל <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "סרטון / קובץ ותמונה ממוזערת נשמרו. תמונה זאת תשמש בסרטון / קובץ."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ התמונה הממוזערת כבר לא שמורה."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ המדיה כבר לא שמורה."
+    SAVED_RECVD_DOC_FILE = "הורדת המסמך הושלמה בהצלחה."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "לא נמצאה תמונה ממוזערת."
+    NO_VOID_FORMAT_FOUND = "אף אחד לא הולך לעזור לך\n<b>YouTubeDL</b> אומר: {}"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> נוסף ל {} עד ל {}."
+    CURENT_PLAN_DETAILS = """פרטי התכנית שלך
+--------
+הid: <code>{}</code>
+תכנית: <a href='https://t.me/SpEcHlDe/599'>{}</a>
+פג ב: {}"""
+    HELP_USER = """כמה דברים שאני יכול לעשות:
+👉 <a href="https://t.me/SpEcHlDe/608">אני יכול להוסיף לכל פורמט וידאו נתמך או קובץ תמונה ממוזערת ולשנות את השם של הקובץ</a>
+👉 <a href="https://t.me/SpEcHlDe/609">להעלות קבצים מקישורי http עם תמונה ממוזערת</a>
+👉 <a href="https://telegram.dog/AnyDLBot">להמיר קובץ וידאו לצפייה ישירה</a>
+👉 <a href="https://telegram.dog/AnyDLBot">להמיר לאודיו בטלגרם המדיה נשלחת  כמסמכי טלגרם</a>
+👉 <a href="https://t.me/SpEcHlDe/610">לשנות שם של קבצים ולהוסיף תמונה ממוזערת</a>
+👉 <a href="https://t.me/SpEcHlDe/625">ליצור תמונה ממוזערת על ידי שליחת 2 תמונות באלבום</a>
+👉 <a href="https://t.me/SpEcHlDe/653">לחתוך סרטונים</a>,  <a href="https://t.me/SpEcHlDe/652">צילום מסך</a> כסרטון טלגרם.
+👉 <a href="https://t.me/SpEcHlDe/657">Extract compressed Telegram media</a>
+👉 <a href="https://t.me/SpEcHlDe/660">Get a Telegram sticker as a Telegram downloadable media</a>
+--------
+תשתמש בפקודה /me בשביל לדעת את התכנית שלך"""
+    REPLY_TO_DOC_GET_LINK = "תשיב על קבצי מדיה בטלגרם בשביל לקבל קישור מהיר להורדה ישירה"
+    REPLY_TO_DOC_FOR_C2V = "תשיב על מדיה בטלגרם להמרה"
+    REPLY_TO_DOC_FOR_SCSS = "תשיב על מדיה בטלגרם בשביל לקבל צילום מסך"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "תשיבו למדיה בטלגרם עם הפקודה /rename בשביל לשנות שם עם תמונה ממוזערת"
+    AFTER_GET_DL_LINK = "קישור להורדה <a href='{}'>נוצר</a> תקף ל {} ימים.\n© @AnyDLBot"
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """תחביר: /trim HH:MM:SS [HH:MM:SS]"""
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "תשתמש בפקודה /downloadmedia בשביל להוריד את זה לזיכרון שלי. \nתשתמש בפקודה  /storageinfo בשביל לראות את המדיה שהורדתי."
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "חיתוך וידאו: {}\nתשתמש בפקודה /clearffmpegmedia בשביל לבטל את שמירת הקובץ אצלי.\nתשתמש בפקודה /trim HH:MM:SS [HH:MM:SS] בהשב על הקובץ שאתה רוצה לחתוך ממנו תמונה או סרטון."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "המדיה כבר שמורה. תשתמש בפקודה /storageinfo בשביל לצפות בפרטים של המדיה."
+    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> כבר לא רשום אצלי."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "תשיב על מדיה בטלגרם (MKV), בשביל לחלץ את הקובץ לצפייה ישירה"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "תשתמש בפקודה /generatecustomthumbnail בהשב על אלבום ואני יוסיף תמונה ממוזערת"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "האלבום חייב לכלול בתוכו רק 2 תמונות בבקשה תשלח את האלבום שוב או שתשלח אלבום עם 2 תמונות."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "כתובת לא חוקית כתובת חוקית חייבת להתחיל ב http:// או https://. אתה יכול לשנות את השם על ידי הקישור | file_name.extension"
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    EXTRACT_ZIP_INTRO_ONE = "קודם תשלח קובץ דחוס, בשביל לחלץ אותו תשתמש בפקודה /unzip בהשב על הקובץ."
+    EXTRACT_ZIP_INTRO_THREE = "מנתח את הקובץ. ⚠️ זה יכול לקחת זמן. "
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    EXTRACT_ZIP_ERRS_OCCURED = התרחשו שגיאות בעיבוד הקובץ הדחוס. אתה יכול לבדוק שוב, אם זה נמשך דווח על זה ל <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_STEP_TWO = """בחר את שם הקובץ להעלאה מהאפשרויות כאן.
+אתה יכול להשתמש בפקודה /rename לאחר קבלת הקובץ בשביל לשנות את השם עם תמונה ממוזערת."""
+    CANCEL_STR = "התהליך בוטל"
+    ZIP_UPLOADED_STR = "מעלה {} קבצים ב {} שניות"
+    FREE_USER_LIMIT_Q_SZE = """לא ניתן לעבד את הקובץ.
+משתמשים בתכנית חינמית יכולים לשלוח רק בקשה אחת כל 30 דקות תשתמשו בפקודה.
+/upgrade בשביל לשדרג או שתחכו 1800 שניות."""
+    SLOW_URL_DECED = "זה נראה כמו כתובת אתר איטית מאוד. אני לא יכול להוריד את הקובץ הזה. אתה יכול לנסות את זה:==> https://shrtz.me/PtsVnf6 ותשלח כתובת מהירה שאני אוכל להעלות לטלגרם, בלי שאני אאט את המהירות למשתמשים אחרים."
+    IFLONG_FILE_NAME = """שם הקובץ מכיל יותר תווים ממה שטלגרם מאפשר המגבלה היא {alimit} תווים.
+בשם הקובץ הזה יש {num} תווים.
+
+<b>שם זה לא מאופשר בקובץ טלגרם!</b>
+©️ <code>@ReNameBot</code>
+תבחר שם פחות ארוך ותנסה שוב!"""
+    YTDL_ERROR_MESSAGE = (
+        "בבקשה תדווח על הבעיה ב https://yt-dl.org/bug . "
+        "ודא שאתה משתמש בגרסה האחרונה; תראה "
+        " https://yt-dl.org/update  כיצד לעדכן. "
+        "תוודא שאתה יוצר איתך קשר ככה youtube-dl עם ה --verbose flag "
+        "ותראה את כל הפלט."
+    )
+    ISOAYD_PREMIUM_VIDEOS = "הסרטון הזה זמין רק למשתמשים רשומים אצלי"
